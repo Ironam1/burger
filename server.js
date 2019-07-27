6c0299710543e7af432828b130c48d0e66f9df52
Ironam1 <ironam1@icloud.com>
2019-07-26 20:20:41 -0400

    added server. have an error in node_modules. need help

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
 const express = require("express");
 const methOver = require("method-override");
 const bodyParser = ("body-parser");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static("./public"));
+
+// override for SQL POST having ?_method=UPDATE
+app.use(methOver("_method"));
+
+// handlebars
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view wngine", "handlebars");
+
+// routes
+const router = require("./controllers/burgers_controller")(app);
+app.use("/", router);
+
+app.listen(PORT, function() {
+    console.log("app listening on PORT: " + PORT);
+});
