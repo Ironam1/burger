c6cc290605eb99b7d15819fce86b7caa439d3444
Ironam1 <ironam1@icloud.com>
2019-07-31 15:54:34 -0400

    added .env with password. now attempting heroku deployment.

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
+require("dotenv").config();
 
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: process.env.DB_PASS,
     database: "burgers_db"
   });
 
